e4d6994f6b69c584e5158b424507d7c7303d4138
Rohit <r.essh.566@gmail.com>
2018-03-23 20:08:53 +0530

    print salutation and the user name

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-x= raw_input ("what is your name?\n ")
-print "hello   " + x
+print "hello"
+spy_name= raw_input("what is your name?  ")
+print "Welcome " + spy_name + " \n glad to have you with us. "
+spy_salutation = raw_input("what should we call you(Mr. or Ms.)?     ")
+spy_name=spy_salutation+" "+spy_name
+print "alright  " + spy_name + "   i'd like to know little more about you....."
