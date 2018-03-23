e51abc8d39962b995ab7bea937d343da168e5c23
Rohit <r.essh.566@gmail.com>
2018-03-23 21:46:12 +0530

    conditon action performed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 print "hello"
 spy_name= raw_input("what is your name?  ")
-print "Welcome " + spy_name + " \n glad to have you with us. "
-spy_salutation = raw_input("what should we call you(Mr. or Ms.)?     ")
-spy_name=spy_salutation+" "+spy_name
-print "alright  " + spy_name + "   i'd like to know little more about you....."
+if len(spy_name)>=2:
+    print "Welcome " + spy_name + " \n glad to have you with us. "
+    spy_salutation = raw_input("what should we call you(Mr. or Ms.)?     ")
+    if spy_salutation == "Mr" or spy_salutation == "Ms" :
+        spy_name=spy_salutation+" "+spy_name
+        print "alright  " + spy_name + "   i'd like to know little more about you....."
+    else: print "sorry, Invalid salutation. "
+else :
+    print "a spy need to have a valid name. Try again please "
