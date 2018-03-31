6ec07892bb6c7e256da380694a112a6783a6c08e
Rohit <r.essh.566@gmail.com>
2018-03-31 18:58:39 +0530

    add status successfuly completed

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from spy_details import spy_name,spy_age,spy_rating      #import a file
 print "hello\n  Let's get started"                       #greeting, initializing
 
-def start_chat(spy_name,spy_age,spy_rating):             #user define function created
+STATUS_MESSAGE=["Available","Sleeping","At the gym","In a meeting","At work","At the movies","Busy","feeling wow!!"] #introducing list
 
+
+def add_status(c_status):
+    if c_status != None:
+        print "Your current status is "+ c_status
+    else:
+        print"You don't have any status currently.."
+    existing_status=raw_input("You want to select from old status? Y/N")
+    if existing_status.upper()=='N':
+        new_status=raw_input('Enter your status : ')
+        if len(new_status)>0:
+            STATUS_MESSAGE.append(new_status)            #adding new status to list..
+    elif existing_status.upper()=='Y':
+        serial_no=1
+        for old_status in STATUS_MESSAGE:
+            print str(serial_no)+old_status
+            serial_no=serial_no+1
+        user_choice=input('Enter your choice :')
+        new_status=STATUS_MESSAGE[user_choice-1]
+    current_status=new_status
+    return current_status
+
+
+def start_chat(spy_name,spy_age,spy_rating):             #user define function created
+    current_status=None
     print 'Here are your options ' +spy_name             #Message given to the user
     show_menu = True                                     #by default value true for validation
     while show_menu:                                     #using loop for multiple times show the same thing
         choice = input('What do you want to do ?\n1.Add a status\n2.Add a friend\n0.Exit ')#choices given to the userinput from the user
         if choice ==1:                                   #conditional Statement
-            print 'This will add the status of the spy'  #Message for the user
+            current_status = add_status(current_status)
+            print'Updated status is ' + current_status
         elif choice==2:                                  #conditional Statement
             print 'This will add a friend'               #Message for the user
         elif choice==0:                                  #conditional Statement
